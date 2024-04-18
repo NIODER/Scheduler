2deb11264452ff706f09b691919d40bd71f22418
NIODER <nioder125@gmail.com>
2024-04-18 15:11:13 +0300

    Add error handling

---
+++ src/Scheduler.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Scheduler.Api.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception is ValidationException ex)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            foreach (var error in ex.Errors)
+            {
+                modelStateDictionary.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+            return ValidationProblem(modelStateDictionary);
+        }
+        return Problem(statusCode: (int)HttpStatusCode.InternalServerError, title: "Something went wrong.");
+    }
+}
