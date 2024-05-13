5425eb89469b02c5643dceeabc09b94d3e8742cd
NIODER <nioder125@gmail.com>
2024-05-13 20:07:00 +0300

    Get task added

---
+++ src/Scheduler.Api/Controllers/ProblemControllers/ProblemsController.cs
@@ -0,0 +1,31 @@
+﻿using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Scheduler.Api.Common.Utils;
+using Scheduler.Application.Problems.Common;
+using Scheduler.Application.Problems.Queries.GetProblem;
+using Scheduler.Contracts.Problems;
+
+namespace Scheduler.Api.Controllers.ProblemControllers;
+
+[ApiController, Route("task"), Authorize]
+public class ProblemsController(IMapper mapper, ISender sender, ILogger logger) : ControllerBase
+{
+    private readonly IMapper _mapper = mapper;
+    private readonly ISender _sender = sender;
+    private readonly ILogger _logger = logger;
+
+    [HttpGet("{taskId}")]
+    public async Task<IActionResult> GetTask(Guid taskId)
+    {
+        var userId = HttpContext.GetExecutorUserId();
+        if (userId is null)
+        {
+            return Forbid();
+        }
+        var query = new GetProblemByIdQuery(userId.Value, taskId);
+        var result = await _sender.Send(query);
+        return result.ActionResult<ProblemResult, ProblemResponse>(_mapper, _logger);
+    }
+}
---
+++ src/Scheduler.Application/Problems/Queries/GetProblem/GetProblemByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Scheduler.Application.Common.Wrappers;
+using Scheduler.Application.Problems.Common;
+
+namespace Scheduler.Application.Problems.Queries.GetProblem;
+
+public record GetProblemByIdQuery(
+    Guid ExecutorId,
+    Guid TaskId
+) : IRequest<ICommandResult<ProblemResult>>;
