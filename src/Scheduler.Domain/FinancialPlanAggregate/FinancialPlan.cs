c307e537c494dbf41db11160d644cf81e8129d8e
NIODER <accurate.account@yandex.ru>
2024-10-14 20:41:48 +0300

    Merge fix

--- src/Scheduler.Domain/FinancialPlanAggregate/FinancialPlan.cs
+++ src/Scheduler.Domain/FinancialPlanAggregate/FinancialPlan.cs
@@ -1,4 +1,4 @@
-using Scheduler.Domain.Common;
+using Scheduler.Domain.Common.DomainDesign;
 using Scheduler.Domain.FinancialPlanAggregate.Entities;
 using Scheduler.Domain.FinancialPlanAggregate.ValueObjects;
 using Scheduler.Domain.GroupAggregate.ValueObjects;
@@ -9,9 +9,9 @@ namespace Scheduler.Domain.FinancialPlanAggregate;
 public class FinancialPlan : Aggregate<FinancialPlanId>
 {
     private readonly List<Charge> _charges = [];
-    public string Title { get; private set; }
+    public string Title { get; set; }
     public UserId CreatorId { get; private set; }
-    public GroupId GroupId { get; private set; }
+    public GroupId? GroupId { get; set; }
 
     private FinancialPlan()
     {
@@ -20,7 +20,7 @@ public class FinancialPlan : Aggregate<FinancialPlanId>
         GroupId = default!;
     }
 
-    private FinancialPlan(FinancialPlanId id, string title, UserId creatorId, GroupId groupId, List<Charge> charges) : base(id)
+    private FinancialPlan(FinancialPlanId id, string title, UserId creatorId, GroupId? groupId, List<Charge> charges) : base(id)
     {
         Title = title;
         _charges = charges;
@@ -32,7 +32,7 @@ public class FinancialPlan : Aggregate<FinancialPlanId>
         string title,
         UserId creatorId,
         List<Charge> charges
-    ) => new(new FinancialPlanId(Guid.NewGuid()), title, creatorId, new(default), charges);
+    ) => new(new FinancialPlanId(Guid.NewGuid()), title, creatorId, null, charges);
 
     public static FinancialPlan CreateGroup(
         string title,
@@ -42,4 +42,6 @@ public class FinancialPlan : Aggregate<FinancialPlanId>
     ) => new(new FinancialPlanId(Guid.NewGuid()), title, creatorId, groupId, charges);
 
     public IReadOnlyCollection<Charge> Charges => _charges.AsReadOnly();
+
+    public bool IsPrivate => GroupId is null;
 }
