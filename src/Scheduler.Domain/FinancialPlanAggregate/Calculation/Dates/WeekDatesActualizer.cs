21c4c4c5cd519f06c08bb561a7fae11c8835582e
NIODER <accurate.account@yandex.ru>
2024-11-22 20:50:04 +0300

    Add week dates actualizer

---
+++ src/Scheduler.Domain/FinancialPlanAggregate/Calculation/Dates/WeekDatesActualizer.cs
@@ -0,0 +1,42 @@
+﻿namespace Scheduler.Domain.FinancialPlanAggregate.Calculation.Dates;
+
+internal class WeekDatesActualizer : IDatesActualizer
+{
+    private const int DAYS_IN_WEEK = 7;
+
+    public bool ActualizeDates(DateTime origin, ref DateTime scheduledDate, ref DateTime expirationDate)
+    {
+        int daysCount = (expirationDate - scheduledDate).Days;
+
+        if (daysCount % DAYS_IN_WEEK != 0)
+        {
+            throw new InvalidDataException($"Can't describe offset between {nameof(scheduledDate)} and {nameof(expirationDate)} as integer number of weeks.");
+        }
+
+        if (scheduledDate <= origin && expirationDate >= origin)
+        {
+            return true;
+        }
+
+        if (expirationDate < origin)
+        {
+            while (expirationDate < origin)
+            {
+                expirationDate = expirationDate.AddDays(daysCount);
+            }
+
+            scheduledDate = expirationDate.AddDays(-daysCount);
+        }
+        else
+        {
+            while (scheduledDate > origin)
+            {
+                scheduledDate = scheduledDate.AddDays(-daysCount);
+            }
+
+            expirationDate = scheduledDate.AddDays(daysCount);
+        }
+
+        return false;
+    }
+}
---
+++ tests/Scheduler.Domain.Tests/DatesActualizerTests/WeeksActualizerTests.cs
@@ -0,0 +1,76 @@
+﻿using Scheduler.Domain.FinancialPlanAggregate.Calculation.Dates;
+
+namespace Scheduler.Domain.Tests.DatesActualizerTests;
+
+public class WeeksActualizerTests
+{
+    [Fact]
+    public void OriginAlreadyInPeriodReturnsTrueTest()
+    {
+        DateTime scheduledDate = DateTime.Parse("2001-01-01 10:10:10,000");
+        DateTime expirationDate = DateTime.Parse("2001-01-08 10:10:10,000");
+        DateTime originDate = DateTime.Parse("2001-01-02 10:10:10,000");
+
+        var actualizer = DatesActualizerFabfic.GetDatesActualizerByRepeatType(Common.Schedule.RepeatType.Weeks);
+
+        Assert.True(actualizer.ActualizeDates(originDate, ref scheduledDate, ref expirationDate));
+        Assert.Equal(DateTime.Parse("2001-01-01 10:10:10,000"), scheduledDate);
+        Assert.Equal(DateTime.Parse("2001-01-08 10:10:10,000"), expirationDate);
+    }
+
+    [Fact]
+    public void OriginGreaterThanExpirationDateInLimitsOfNextPeriodCorrectsDatesAndReturnsFalseTest()
+    {
+        DateTime scheduledDate = DateTime.Parse("2001-01-01 10:10:10,000");
+        DateTime expirationDate = DateTime.Parse("2001-01-08 10:10:10,000");
+        DateTime originDate = DateTime.Parse("2001-01-10 10:10:10,000");
+
+        var actualizer = DatesActualizerFabfic.GetDatesActualizerByRepeatType(Common.Schedule.RepeatType.Weeks);
+
+        Assert.False(actualizer.ActualizeDates(originDate, ref scheduledDate, ref expirationDate));
+        Assert.Equal(DateTime.Parse("2001-01-08 10:10:10,000"), scheduledDate);
+        Assert.Equal(DateTime.Parse("2001-01-15 10:10:10,000"), expirationDate);
+    }
+
+    [Fact]
+    public void OriginGreaterThanExpirationDateNotInLimitsOfNextPeriodCorrectsDatesAndReturnsFalseTest()
+    {
+        DateTime scheduledDate = DateTime.Parse("2001-01-01 10:10:10,000");
+        DateTime expirationDate = DateTime.Parse("2001-01-08 10:10:10,000");
+        DateTime originDate = DateTime.Parse("2001-01-17 10:10:10,000");
+
+        var actualizer = DatesActualizerFabfic.GetDatesActualizerByRepeatType(Common.Schedule.RepeatType.Weeks);
+
+        Assert.False(actualizer.ActualizeDates(originDate, ref scheduledDate, ref expirationDate));
+        Assert.Equal(DateTime.Parse("2001-01-15 10:10:10,000"), scheduledDate);
+        Assert.Equal(DateTime.Parse("2001-01-22 10:10:10,000"), expirationDate);
+    }
+
+    [Fact]
+    public void OriginLessThanExpirationDateInLimitsOfNextPeriodCorrectsDatesAndReturnsFalseTest()
+    {
+        DateTime scheduledDate = DateTime.Parse("2001-01-15 10:10:10,000");
+        DateTime expirationDate = DateTime.Parse("2001-01-22 10:10:10,000");
+        DateTime originDate = DateTime.Parse("2001-01-10 10:10:10,000");
+
+        var actualizer = DatesActualizerFabfic.GetDatesActualizerByRepeatType(Common.Schedule.RepeatType.Weeks);
+
+        Assert.False(actualizer.ActualizeDates(originDate, ref scheduledDate, ref expirationDate));
+        Assert.Equal(DateTime.Parse("2001-01-08 10:10:10,000"), scheduledDate);
+        Assert.Equal(DateTime.Parse("2001-01-15 10:10:10,000"), expirationDate);
+    }
+
+    [Fact]
+    public void OriginLessThanExpirationDateNotInLimitsOfNextPeriodCorrectsDatesAndReturnsFalseTest()
+    {
+        DateTime scheduledDate = DateTime.Parse("2001-01-15 10:10:10,000");
+        DateTime expirationDate = DateTime.Parse("2001-01-22 10:10:10,000");
+        DateTime originDate = DateTime.Parse("2001-01-02 10:10:10,000");
+
+        var actualizer = DatesActualizerFabfic.GetDatesActualizerByRepeatType(Common.Schedule.RepeatType.Weeks);
+
+        Assert.False(actualizer.ActualizeDates(originDate, ref scheduledDate, ref expirationDate));
+        Assert.Equal(DateTime.Parse("2001-01-01 10:10:10,000"), scheduledDate);
+        Assert.Equal(DateTime.Parse("2001-01-08 10:10:10,000"), expirationDate);
+    }
+}
