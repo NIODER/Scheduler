c47c6f9d73ff75fa84914793da1358e63e00d29b
NIODER <accurate.account@yandex.ru>
2024-12-23 12:04:11 +0300

    Commented ctor

--- tests/Scheduler.Infrastructure.Tests/DatabaseTests/DatabaseCreatingTests.cs
+++ tests/Scheduler.Infrastructure.Tests/DatabaseTests/DatabaseCreatingTests.cs
@@ -1,6 +1,4 @@
-using Microsoft.EntityFrameworkCore;
 using Scheduler.Infrastructure.Persistance;
-using Scheduler.Infrastructure.Persistance.Interceptors;
 
 namespace Scheduler.Infrastructure.Tests.DatabaseTests;
 
@@ -8,15 +6,16 @@ public class DatabaseCreatingTests
 {
     private readonly SchedulerDbContext context = null!;
 
-    public DatabaseCreatingTests()
-    {
-        context = new SchedulerDbContext(
-            new DbContextOptionsBuilder<SchedulerDbContext>()
-            .UseNpgsql("Username=postgres;Host=localhost;Port=5432;Password=123123")
-            .Options,
-            new PublishDomainEventsInterceptor(null)
-        );
-    }
+    // TODO: Add publisher through DI
+    //public DatabaseCreatingTests()
+    //{
+    //    context = new SchedulerDbContext(
+    //        new DbContextOptionsBuilder<SchedulerDbContext>()
+    //        .UseNpgsql("Username=postgres;Host=localhost;Port=5432;Password=123123")
+    //        .Options,
+    //        new PublishDomainEventsInterceptor(null)
+    //    );
+    //}
 
     [Fact]
     public void CreateDatabase()
